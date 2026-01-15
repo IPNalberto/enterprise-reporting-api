8d0560001780f3824a59f1251c50f2adccc9132c
Alberto Hernandez <behermon@hotmail.com>
2026-01-15 17:46:48 -0600

    Fist update

--- src/main/java/org/example/sistemareportes/SistemaReportesApplication.java
+++ src/main/java/org/example/sistemareportes/SistemaReportesApplication.java
@@ -1,4 +1,4 @@
-package org.example.sistemareportes;
+package com.alberto.sistemareportes;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/org/example/sistemareportes/SistemaReportesApplicationTests.java
+++ src/test/java/org/example/sistemareportes/SistemaReportesApplicationTests.java
@@ -1,4 +1,4 @@
-package org.example.sistemareportes;
+package com.alberto.sistemareportes;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
