a15f713445266d2319df27daeab0677005783e0b
Alberto Hernandez <behermon@hotmail.com>
2026-01-15 14:35:54 -0600

    Primer commit

---
+++ src/main/java/org/example/sistemareportes/SistemaReportesApplication.java
@@ -0,0 +1,13 @@
+package org.example.sistemareportes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaReportesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SistemaReportesApplication.class, args);
+    }
+
+}
